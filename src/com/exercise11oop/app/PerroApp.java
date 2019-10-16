3017e1c3542e3e93cccfc6981ff7d9e55bdfedbf
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-10-16 15:58:20 -0500

    Cat class created.

--- src/com/exercise11oop/app/PerroApp.java
+++ src/com/exercise11oop/app/PerroApp.java
@@ -1,19 +1,19 @@
 package com.exercise11oop.app;
 
-import com.exercise11oop.model.Perro;
+import com.exercise11oop.model.Dog;
 
 public class PerroApp 
 {
 
 	public static void main(String[] args) 
 	{	
-		
 		//Way 1
-		Perro pluto;
-		pluto = new Perro();
+		Dog pluto;
+		pluto = new Dog();
 		
-		//Way 2
-		Perro tribilin = new Perro();
+		System.out.println("Pluto's age:"+pluto.age);
+		System.out.println("Pluto's height:"+pluto.height);
+		System.out.println("PLuto's "+pluto.id);
 		
 		pluto.age = 15;
 		pluto.height = 50;
@@ -22,5 +22,13 @@ public class PerroApp
 		System.out.println("Pluto's age:"+pluto.age);
 		System.out.println("Pluto's height:"+pluto.height);
 		System.out.println("PLuto's "+pluto.id);
+		
+		Dog firulais = new Dog(1, "Firulais", "Labrador",5,60.0);
+		
+		System.out.println("Firulais age:"+firulais.age);
+		System.out.println("Firulais height:"+firulais.height);
+		System.out.println("Firulais id:"+firulais.id);
+		
+		Dog lucho = new Dog(1, "Lucho");
 	}
 }
