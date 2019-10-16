30cfb7c8e4975762c098ebfc3d6bfcd0cdd77b5f
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-10-16 15:26:42 -0500

    Perro class created

---
+++ src/com/exercise11oop/model/Perro.java
@@ -0,0 +1,16 @@
+package com.exercise11oop.model;
+
+public class Perro
+{
+	public int id;
+	private String name;
+	protected String breed;
+	public int age;
+	public double height;
+	
+	public Perro(int id, String name, String breed, int age, double height)
+	{
+		
+	}
+	public Perro(){}
+}
